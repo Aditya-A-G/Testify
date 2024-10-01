fb641e51311ee8b597cbcd6b11bb3fd596eb05f5
Aditya Ghadge <adityaghadge360@gmail.com>
2024-10-01 20:10:03 +0530

    allow user to select region for test
    Signed-off-by: Aditya Ghadge <adityaghadge360@gmail.com>

--- apps/web/components/InputForm.tsx
+++ apps/web/components/InputForm.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-
 import { toast } from "@/hooks/use-toast";
 import { Button } from "@/components/ui/button";
+import { Check, ChevronsUpDown } from "lucide-react";
 import {
   Form,
   FormControl,
@@ -13,11 +14,35 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 import { Input } from "@/components/ui/input";
+
+const regions = [
+  { label: "us", value: "us" },
+  { label: "eu", value: "eu" },
+  { label: "asia", value: "asia" },
+  { label: "india", value: "india" },
+] as const;
 
 const FormSchema = z.object({
   websiteUrl: z.string().url({
     message: "Please enter a valid URL.",
+  }),
+  region: z.string({
+    required_error: "Please select a region",
   }),
 });
 
@@ -26,6 +51,7 @@ export function InputForm() {
     resolver: zodResolver(FormSchema),
     defaultValues: {
       websiteUrl: "",
+      region: "us",
     },
   });
 
@@ -82,6 +108,64 @@ export function InputForm() {
               <FormControl>
                 <Input placeholder="https://adityacodes.tech" {...field} />
               </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="region"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      className={cn(
+                        "w-[200px] justify-between",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value
+                        ? regions.find((region) => region.value === field.value)
+                            ?.label
+                        : "Select region"}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] p-0">
+                  <Command>
+                    <CommandInput placeholder="Search region..." />
+                    <CommandList>
+                      <CommandEmpty>No regionr found.</CommandEmpty>
+                      <CommandGroup>
+                        {regions.map((region) => (
+                          <CommandItem
+                            value={region.label}
+                            key={region.value}
+                            onSelect={() => {
+                              form.setValue("region", region.value);
+                            }}
+                          >
+                            <Check
+                              className={cn(
+                                "mr-2 h-4 w-4",
+                                region.value === field.value
+                                  ? "opacity-100"
+                                  : "opacity-0"
+                              )}
+                            />
+                            {region.label}
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              </Popover>
               <FormMessage />
             </FormItem>
           )}
