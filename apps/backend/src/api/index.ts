6774dae41bc62b6d6e94630e09fc99c8524ca55d
Aditya Ghadge <adityaghadge360@gmail.com>
2024-10-02 20:49:55 +0530

    create /results api to receive and store results
    Signed-off-by: Aditya Ghadge <adityaghadge360@gmail.com>

--- apps/backend/src/api/index.ts
+++ apps/backend/src/api/index.ts
@@ -4,6 +4,7 @@ import MessageResponse from "../interfaces/MessageResponse";
 import { sendToRegionQueue } from "../config/queue";
 import { redis } from "../config/redis";
 import { JobModel } from "./models/jobModel";
+import { PerformanceTestResultModel } from "./models/performanceTestResultModel";
 
 const router = express.Router();
 
@@ -35,6 +36,86 @@ router.post("/tests", async (req, res) => {
     jobId: jobId,
     status: "pending",
   });
+});
+
+router.post("/results", async (req, res) => {
+  const {
+    jobId,
+    region,
+    loadTime,
+    domContentLoaded,
+    ttfb,
+    firstPaintTime,
+    firstContentfulPaintTime,
+    timeToInteractive,
+    numberOfRequests,
+    pageSize,
+    status,
+    error,
+  } = req.body;
+
+  try {
+    if (status === "failed") {
+      await JobModel.findOneAndUpdate(
+        { jobId },
+        {
+          $set: {
+            status: "failed",
+            completedAt: new Date(),
+          },
+        }
+      );
+
+      await redis.set(jobId, "failed");
+
+      return res.status(400).json({
+        message: "Test failed",
+        jobId,
+        status: "failed",
+        error,
+      });
+    }
+
+    const performanceTestResult = new PerformanceTestResultModel({
+      jobId,
+      region,
+      loadTime,
+      domContentLoaded,
+      firstByteTime: ttfb,
+      firstPaintTime,
+      firstContentfulPaintTime,
+      timeToInteractive,
+      numberOfRequests,
+      pageSize,
+    });
+
+    await performanceTestResult.save();
+
+    await JobModel.findOneAndUpdate(
+      { jobId },
+      {
+        $set: {
+          status: "completed",
+          completedAt: new Date(),
+          results: { performanceTest: performanceTestResult._id },
+        },
+      }
+    );
+
+    await redis.set(jobId, "completed");
+
+    res.status(200).json({
+      message: "Results saved successfully",
+      jobId,
+      status: "completed",
+    });
+  } catch (error) {
+    console.error("Error saving results:", error);
+    res.status(500).json({
+      message: "Failed to save results",
+      error: error,
+    });
+  }
 });
 
 export default router;
