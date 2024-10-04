bfff43b77f4e030ccca85c0b7670ae1282c551cc
Aditya Ghadge <adityaghadge360@gmail.com>
2024-10-05 02:46:03 +0530

    add cors config

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -6,6 +6,7 @@ import * as middlewares from "./middlewares";
 import api from "./api";
 import MessageResponse from "./interfaces/MessageResponse";
 import { connectWithRetry } from "./config/mongoDB";
+import { NODE_ENV } from "./config/config";
 
 const app = express();
 
@@ -13,7 +14,14 @@ connectWithRetry();
 
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      NODE_ENV === "production"
+        ? "https://testify.adityacodes.tech"
+        : "http://localhost:300",
+  })
+);
 app.use(express.json());
 
 app.get<{}, MessageResponse>("/", (req, res) => {
