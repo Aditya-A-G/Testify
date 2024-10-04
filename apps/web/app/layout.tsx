826eba65be760cd11661c54f07d587888e5fef73
Aditya Ghadge <adityaghadge360@gmail.com>
2024-10-05 02:37:50 +0530

    add vercel analytics for web

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -56,6 +57,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <main>{children}</main>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
