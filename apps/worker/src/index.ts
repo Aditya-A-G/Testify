42bdd727037c12757989df268f036aa7ccbee334
Aditya Ghadge <adityaghadge360@gmail.com>
2024-10-05 16:50:16 +0530

    update puppeteer launch config

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -27,6 +27,7 @@ interface PaintMetrics {
 async function initBrowser() {
   if (!browser) {
     browser = await puppeteer.launch({
+      headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     console.log("Browser instance initialized");
