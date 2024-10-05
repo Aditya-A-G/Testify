fc78b6d9a05b8775e398a178d03ce9d053f75520
Aditya Ghadge <adityaghadge360@gmail.com>
2024-10-06 01:02:11 +0530

    update backend port

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 443;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
